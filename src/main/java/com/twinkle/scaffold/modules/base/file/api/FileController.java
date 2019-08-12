dc82c86775558466b5878825dc5db7e9edb15ce6
Administrator <Administrator@D5SQV2K2ASI1CSU>
2019-08-12 22:57:51 +0800

    添加流媒体的分段下载

--- src/main/java/com/twinkle/scaffold/modules/base/file/api/FileController.java
+++ src/main/java/com/twinkle/scaffold/modules/base/file/api/FileController.java
@@ -1,9 +1,16 @@
 package com.twinkle.scaffold.modules.base.file.api;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.sql.rowset.serial.SerialException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -106,4 +113,80 @@ public class FileController {
                 .body(new InputStreamResource(simpleFile.getInputStream()));  
     }
     
+    @ApiOperation(value = "文件下载", notes = "支持断点续传的文件下载,适合连接流媒体",produces="application/octet-stream")
+    @ApiImplicitParams({
+        @ApiImplicitParam(name = "id",value="文件ID",required = true)})
+    @GetMapping(value="/auth/v1/file/singlefile/{id}/ranges")
+    public void noauthGetfile(@PathVariable("id") String id,HttpServletRequest request, HttpServletResponse response) throws SQLException {
+        SimpleFile simpleFile = jdbcFileManager.getFileById(id);
+        InputStream inputStream = simpleFile.getInputStream();
+        long size = simpleFile.getSize();
+        String name = simpleFile.getName();
+        simpleFile.setInputStream(inputStream);
+        response.setHeader("Content-Disposition", String.format("attachment; filename=\"%s\"", name));  
+        response.setHeader("Cache-Control","no-cache, no-store, max-age=0, must-revalidate");
+        response.setContentType(simpleFile.getContentType());
+        long fromPos = 0;
+        long downSize = size;
+        if (request.getHeader("Range") != null) {
+            long toPos = 0;
+            String range = request.getHeader("Range");
+            String bytes = range.replaceAll("bytes=", "");
+            String[] ary = bytes.split("-");
+            fromPos = Long.parseLong(ary[0]);
+            if (ary.length == 1) {
+                toPos = fromPos+1024*1024*2;
+                if(toPos > size){
+                    toPos = size;
+                }
+            }else{
+                toPos = Long.parseLong(ary[1]);
+            }
+            if (toPos > fromPos) {
+                downSize = (int) (toPos - fromPos);
+            }
+            toPos = toPos -1;
+            response.setHeader("Accept-Ranges", "bytes");
+            response.setHeader("Content-Range", "bytes "+fromPos+"-"+toPos+"/"+size);
+            response.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT);
+        }
+        response.setHeader("Content-Length", downSize + "");
+        try {
+            inputStream.skip(fromPos);
+            // 缓冲区大小
+            int bufLen = (int) (size < 2048 ? size : 2048);
+            byte[] buffer = new byte[bufLen];
+            int num;
+            int count = 0; // 当前写到客户端的大小
+            OutputStream out = response.getOutputStream();
+            while ((num = inputStream.read(buffer)) != -1) {
+                out.write(buffer);
+                count += num;
+                //处理最后一段，计算不满缓冲区的大小
+                if (size - count < bufLen) {
+                    bufLen = (int) (size-count);
+                    if(bufLen==0){
+                        break;
+                    }
+                    buffer = new byte[bufLen];
+                }
+            }
+            out.flush();
+        } catch (IOException e) {
+            log.warn(e.getMessage());
+        } finally {
+            try {
+                response.getOutputStream().close();
+            } catch (IOException e) {
+                log.warn(e.getMessage());
+            }
+            if (null != inputStream) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    log.warn(e.getMessage());
+                }
+            }
+        }
+    }
 }
--- 规范与模块介绍/file模块介绍.md
+++ 规范与模块介绍/file模块介绍.md
@@ -5,12 +5,12 @@
     可以对接第三方储存服务，进行对接实现
 2，api接口主要实现了
 ---
-    文件上传、下载和删除
+    文件上传、删除、下载和适合于流媒体的分段下载
 3，部署上需要考虑，服务器文件上传的大小限制
 ---
     数据库目前使用的是mediumblob，单文件最大16M
     spring:
       servlet:
         multipart:
-          max-file-size: 1024MB
-          max-request-size: 1024MB
+          max-file-size: 16MB
+          max-request-size: 16MB
