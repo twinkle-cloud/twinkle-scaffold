4b498b042a3525859369982e0b1dbf5f13f8bf9b
Administrator <Administrator@D5SQV2K2ASI1CSU>
2019-08-19 21:49:27 +0800

    moduleA 新增包

---
+++ src/main/java/com/twinkle/scaffold/modules/biz/moduleA/listener/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 监听业务事件，实现业务解耦 <br/>
+ * Date:    2019年8月19日 下午9:47:05 <br/>
+ *
+ * @author yukang
+ * @see
+ * @since JDK 1.8
+ */
+package com.twinkle.scaffold.modules.biz.moduleA.listener;
---
+++ src/main/java/com/twinkle/scaffold/modules/biz/moduleA/task/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 定时任务，写在这里 <br/>
+ * Date:    2019年8月19日 下午9:48:34 <br/>
+ *
+ * @author yukang
+ * @see
+ * @since JDK 1.8
+ */
+package com.twinkle.scaffold.modules.biz.moduleA.task;
