d16f0513fcdcdbe28168fa5ad7010a607dfec034
Administrator <Administrator@D5SQV2K2ASI1CSU>
2019-08-24 18:50:42 +0800

    add 2 packages

---
+++ src/main/java/com/twinkle/scaffold/modules/base/datacenter/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 跨模块数据查询 ，写在该包下面<br/>
+ * Date:    2019年8月24日 下午5:44:56 <br/>
+ *
+ * @author yukang
+ * @see
+ * @since JDK 1.8
+ */
+package com.twinkle.scaffold.modules.base.datacenter;
---
+++ src/main/java/com/twinkle/scaffold/modules/biz/moduleA/strategy/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * 该包下放置各类策略 <br/>
+ * 每类策略下一般会有策略选择器 <br/>
+ * 不同层次的对业务逻辑进行抽象和梳理 <br/>
+ * Date:    2019年8月24日 下午6:30:10 <br/>
+ *
+ * @author yukang
+ * @see
+ * @since JDK 1.8
+ */
+package com.twinkle.scaffold.modules.biz.moduleA.strategy;
